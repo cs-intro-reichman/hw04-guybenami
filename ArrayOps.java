d24fa0f8b85e384e012d73143df74f6e67a38110
guybenami <guy.benami@post.runi.ac.il>
2024-01-10 17:34:07 +0200

    firstTry

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,71 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        int arr[] = {1,2,3,3,2,1,3,2,3,2,4,1,3,2};
+        int arr2[] = {5,4,4,3,3,3,3,3,4,1};
+        System.out.println(isSorted(arr2));
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int n = array.length;
+        boolean check[] = new boolean[n+1];
+        for(int i=0 ; i<n ; i++) 
+            check[i] = false;
+        for(int i=0 ; i<n ; i++) {
+            check[array[i]] = true;
+        }
+        int j = 0;
+        while(check[j])
+            j++;
+        return j;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int max = -2147483648;
+        int max2 = -2147483648;
+        int maxIndex = 0;
+        for(int i=0 ; i<array.length ; i++) {
+            if(max<array[i]) {
+                max = array[i];
+                maxIndex = i;
+            }
+        }
+        for(int i=0 ; i<array.length ; i++) {
+            if(max2 < array[i] && i != maxIndex)
+                max2 = array[i];
+        }
+        return max2;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        boolean check = true;
+        for(int i=0 ; i<array1.length ; i++) {
+            for(int j=0 ; j<array2.length ; j++) {
+                if(array1[i]==array2[j])
+                    break;
+                else if(j==array2.length-1)
+                    check = false;
+            }
+        }
+        for(int i=0 ; i<array2.length ; i++) {
+            for(int j=0 ; j<array1.length ; j++) {
+                if(array2[i]==array1[j])
+                    break;
+                else if(j==array1.length-1)
+                    check = false;
+            }
+        }
+        return check;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        boolean increase=true, decrease=true;
+        for(int i=0 ; i<array.length-1 && (increase || decrease); i++) {
+            if(array[i]>array[i+1] && increase)
+                increase = false;
+            if(array[i]<array[i+1] && decrease)
+                decrease = false;
+        }
+        return increase || decrease;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -26,17 +26,82 @@ public class StringOps {
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String newStr = "";
+        for(int i=0 ; i<string.length() ; i++) {
+            if(isVowel(string.charAt(i))) {
+                if(string.charAt(i)>90)
+                    newStr += (char)(string.charAt(i)-32);
+                else
+                    newStr += string.charAt(i);
+            }
+            else {
+                if(string.charAt(i)== ' ' || string.charAt(i)>90)
+                    newStr += string.charAt(i);
+                else
+                    newStr += (char)(string.charAt(i)+32);
+            }
+        }
+        return newStr;
+    }
+
+    public static boolean isVowel(char ch) {
+        String vowels = "aeiouAEIOU";
+        return vowels.indexOf(ch) != -1;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String newStr = "";
+        indexOfSpaces(string);
+        string = lowerCase(string);
+        for(int i=0 ; i<string.length() ; i++) {
+            if(!space_indexes[i]) {
+                if(i==0)
+                    newStr += string.charAt(i);
+                else if(!space_indexes[i-1])
+                    newStr += string.charAt(i);
+                else if(newStr != "")
+                    newStr += (char)(string.charAt(i) - 32);
+                else
+                    newStr += string.charAt(i);
+            }
+        }
+        return newStr;
+    }
+
+    static boolean[] space_indexes = new boolean[1000000];
+    public static void indexOfSpaces(String str) {
+        for(int i=0 ; i<space_indexes.length ; i++)
+            space_indexes[i] = false;
+        for(int i=0 ; i<str.length() ; i++){
+            if(str.charAt(i) == ' ')
+                space_indexes[i] = true;
+        }
+    }
+
+    public static String lowerCase(String str) {
+        String newStr = "";
+        for(int i=0 ; i<str.length() ; i++)
+            if(str.charAt(i)<91 && str.charAt(i) != ' ')
+                newStr += (char)(str.charAt(i)+32);
+            else
+                newStr += str.charAt(i);
+        return newStr;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int counter = 0;
+        for(int i=0 ; i<string.length() ; i++) {
+            if(string.charAt(i)==chr)
+                counter++;
+        }
+        int j=0;
+        int[] chrIndex = new int[counter];
+        for(int i=0 ; i<string.length() ; i++) {
+            if(string.charAt(i)==chr) {
+                chrIndex[j] = i;
+                j++;
+            }
+        }
+        return chrIndex;
     }
 }
